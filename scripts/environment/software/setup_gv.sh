6e27e48459cbcde4476404f4ba2e9df8b4f2c9ea
masterdesky <masterdesky@gmail.com>
2022-05-10 21:37:55 +0200

    gadgetviewer still fails for gcc 11.2

--- scripts/environment/software/setup_gv.sh
+++ scripts/environment/software/setup_gv.sh
@@ -9,10 +9,14 @@ if [[ ${DLOAD_GV} = true ]]; then
 
     mkdir -p ${BUILDDIR}
 
-    wget "https://github.com/jchelly/gadgetviewer/releases/download/${GV_VER}/gadgetviewer-${GV_VER}.tar.gz" -P ${BUILDDIR}
-    tar -xzvf ${BUILDDIR}/gadgetviewer-${GV_VER}.tar.gz -C ${BUILDDIR}
-    rm -f ${BUILDDIR}/gadgetviewer-${GV_VER}.tar.gz
-    mv ${BUILDDIR}/gadgetviewer-${GV_VER} ${BUILDDIR}/GADGETViewer-${GV_VER}
+    # Development version
+    git clone https://github.com/jchelly/gadgetviewer ${BUILDDIR}/GADGETViewer-${GV_VER}
+    
+    # Release version
+    #wget "https://github.com/jchelly/gadgetviewer/releases/download/${GV_VER}/gadgetviewer-${GV_VER}.tar.gz" -P ${BUILDDIR}
+    #tar -xzvf ${BUILDDIR}/gadgetviewer-${GV_VER}.tar.gz -C ${BUILDDIR}
+    #rm -f ${BUILDDIR}/gadgetviewer-${GV_VER}.tar.gz
+    #mv ${BUILDDIR}/gadgetviewer-${GV_VER} ${BUILDDIR}/GADGETViewer-${GV_VER}
   fi
 fi
 
@@ -29,10 +33,13 @@ then
         make uninstall |& tee >(ts "[%x %X]" > ${BUILDDIR}/GADGETViewer-${GV_VER}/mu.log)
         make clean |& tee >(ts "[%x %X]" > ${BUILDDIR}/GADGETViewer-${GV_VER}/cl.log)
     fi
+
+    # Devel versions requires the configure script to be generated first
+    if [[ ! -f ${BUILDDIR}/GADGETViewer-${GV_VER}/configure ]]; then
+      source ${BUILDDIR}/GADGETViewer-${GV_VER}/autogen.sh
+    fi
     ./configure --prefix=${INSTALLDIR}/GADGETViewer-${GV_VER} \
                 CFLAGS='-g -O3 -Wall -std=c17' \
-                CXXFLAGS='-g -O3 -Wall -std=c++17 ' \
-                FFLAGS='-fallow-argument-mismatch' \
                 FCFLAGS='-fallow-argument-mismatch' \
                 |& tee >(ts "[%x %X]" > ${BUILDDIR}/GADGETViewer-${GV_VER}/c.log)
     make -j${N_CPUS} |& tee >(ts "[%x %X]" > ${BUILDDIR}/GADGETViewer-${GV_VER}/m.log)
