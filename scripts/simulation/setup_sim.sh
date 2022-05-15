5e92175e007346338196c04a30b359b5336d28a5
masterdesky <masterdesky@gmail.com>
2022-05-15 23:31:09 +0200

    added several new comments to setup_sim.sh

--- scripts/simulation/setup_sim.sh
+++ scripts/simulation/setup_sim.sh
@@ -11,27 +11,33 @@
 
 
 # Check if parameters are correctly set
-##  1. NPART
+##  1. NPART : - Number of simulated particles in N-body simulations
+##             - Number of simulated HD volumes in HD simulations 
 if [[ ${NPART} < 1 ]]; then
-  echo "[ERROR] NPART should be an integer greater than 1!" | ts "[%x %X]"
+  echo "[ERROR] NPART should be an integer greater than 1!" \
+  | ts "[%x %X]"
   clean_up
   exit 2
 fi
-##  2. LBOX
+##  2. LBOX : - Boxsize of periodic, cubical simulations
 if [[ ${LBOX} < 0 ]]; then
-  echo "[ERROR] LBOX should be positive!" | ts "[%x %X]"
+  echo "[ERROR] LBOX should be positive!" \
+  | ts "[%x %X]"
   clean_up
   exit 2
 fi
-##  3. LBOX_PER
+##  3. LBOX_PER : - Boxsize of the tiled, periodic, cubical simulations. Tiling
+##                  could be used to generate glasses of bigger size or 
 if [[ ${LBOX_PER} < 0 || ${LBOX_PER} < ${LBOX} ]]; then
-  echo "[ERROR] LBOX_PER should be positive and greater than LBOX!" | ts "[%x %X]"
+  echo "[ERROR] LBOX_PER should be positive and greater than LBOX!" \
+  | ts "[%x %X]"
   clean_up
   exit 2
 fi
 ##  4.  MBINS
 if [[ ${MBINS} < 1 ]]; then
-  echo "[ERROR] MBINS should be an integer greater than 1!" | ts "[%x %X]"
+  echo "[ERROR] MBINS should be an integer greater than 0!" \
+  | ts "[%x %X]"
   clean_up
   exit 2
 ## If there is only a single mass bin, then there's no need to set the
